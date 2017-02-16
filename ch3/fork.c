bea9eea97579282d713b40fcc7a281145836f411
donnice <402696415@qq.com>
2017-02-16 00:00:42 -0600

    fork

---
+++ ch3/fork.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main()
+{
+  pid_t pid;
+  printf("Now only one process\n");
+  printf("Calling fork....\n");
+  pid = fork();
+  if(!pid)
+    printf("I am the child\n");
+  else if(pid > 0)
+    printf("I am the parent, child has pid %d\n",pid);
+  else
+    printf("Fork fail!\n");
+  
+  return 0;
+}
