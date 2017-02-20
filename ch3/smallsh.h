9caea57c4aa267284649fd5b2bc6f22a8a375835
donnice <402696415@qq.com>
2017-02-19 21:35:29 -0600

    smallsh

---
+++ ch3/smallsh.h
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+#define EOL 1 /*End of line*/
+#define ARG 2
+#define AMPERSAND 3
+#define SEMICOLON 4
+#define MAXARG 512 /* maximum args */
+#define MAXBUF 512
+#define FOREGROUND 0
+#define BACKGROUND 1
