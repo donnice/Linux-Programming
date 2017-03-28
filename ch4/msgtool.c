40599c9c3da362fe8a9734bf34f22366675e92c7
402696415@qq.com <402696415@qq.com>
2017-03-28 01:51:47 -0500

    msgtool

--- ch4/msgtool.c
+++ ch4/msgtool.c
@@ -31,7 +31,7 @@ int main(int argc, char *argv[])
   key = ftok(".",'m');
 
   /* Open the queue - create if necessary */
-  if((msgqueue_id=msgget(key,IPC_CREAT|0660))=-1){
+  if((msgqueue_id=msgget(key,IPC_CREAT|0660))==-1){
     perror("msgget");
     exit(1);
   }
@@ -54,4 +54,53 @@ int main(int argc, char *argv[])
 
 void send_message(int qid, struct mymsgbuf* qbuf, long type,char *text)
 {
-  
+  /* Send a message to queue */
+  printf("Sending a message...\n");
+  qbuf->mtype=type;
+  strcpy(qbuf->mtext,text);
+
+  if((msgsnd(qid,(struct msgbuf*)qbuf,
+	     strlen(qbuf->mtext)+1,0))==-1)
+    {
+      perror("msgsnd");
+      exit(1);
+    }
+}
+
+void read_message(int qid, struct mymsgbuf *qbuf, long type)
+{
+  /* Read a message from queue */
+  printf("Reading a message... \n");
+  qbuf->mtype=type;
+  msgrcv(qid,(struct msgbuf*)qbuf, MAX_SEND_SIZE, type, 0);
+  printf("Type:%ld Text: %s\n",qbuf->mtype,qbuf->mtext);
+}
+
+void remove_queue(int qid)
+{
+  /* Remove the queue */
+  msgctl(qid,IPC_RMID, 0);
+}
+
+void change_queue_mode(int qid, char *mode)
+{
+  struct msqid_ds myqueue_ds;
+
+  /* Get current info */
+  msgctl(qid,IPC_STAT,&myqueue_ds);
+
+  /* Convert and load the mode */
+  sscanf(mode,"%ho",&myqueue_ds.msg_perm.mode);
+
+  /* Update the mode */
+  msgctl(qid,IPC_SET,&myqueue_ds);
+}
+
+void usage(void)
+{
+  fprintf(stderr,"msgtool - A utility for tinkering with msg queues\n");
+  fprintf(stderr,"\nUSAGE:msgtool (s)end <type><messagetext>\n");
+  fprintf(stderr,"                (r)recv <type>\n");
+  fprintf(stderr,"                (m)ode <octal mode>\n");
+  exit(1);
+}
