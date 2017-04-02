41961b55dd37bd945e40529dc751a935c1f7597d
402696415@qq.com <402696415@qq.com>
2017-04-01 22:44:13 -0500

    shared memory

--- ch4/shmtool.c
+++ ch4/shmtool.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
@@ -57,4 +58,50 @@ int main(int argc, char* argv[])
 
     default:usage();
     }
+}
+
+void writeshm(int shmid, char *segptr, char *text)
+{
+  strcpy(segptr,text);
+  printf("Done...\n");
+}
+
+void readshm(int shmid,char *segptr)
+{
+  printf("segptr:%s\n",segptr);
+}
+
+void removeshm(int shmid)
+{
+  shmctl(shmid,IPC_RMID,0);
+  printf("Shared mem segment marked for deletion\n");
+}
+
+void changemode(int shmid, char *mode)
+{
+  struct shmid_ds myshmds;
+
+  /* Get current values for internal ds */
+  shmctl(shmid,IPC_STAT,&myshmds);
+
+  /* Display old permissions */
+  printf("Old permissions were: %o\n",myshmds.shm_perm.mode);
+
+  /* Convert and load the mode */
+  sscanf(mode,"%o",&myshmds.shm_perm.mode);
+
+  /*update mode */
+  shmctl(shmid,IPC_SET,&myshmds);
+
+  printf("New permissions are:%o\n",myshmds.shm_perm.mode);
+}
+
+void usage()
+{
+  fprintf(stderr,"shmtool - A utility for tinkering with sm\n");
+  fprintf(stderr,"\nUSAGE: shmtool (w)rite <text>\n");
+  fprintf(stderr,"                 (r)ead\n");
+  fprintf(stderr,"                 (d)elete\n");
+  fprintf(stderr,"                 (m)ode change <octal mode>\n");
+  exit(1);
 }
