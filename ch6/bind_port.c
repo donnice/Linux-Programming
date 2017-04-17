59c5c9adfd4b25f2d7a10efcc41f6f5f0fb58ba3
402696415@qq.com <402696415@qq.com>
2017-04-17 00:36:53 -0500

    bind

---
+++ ch6/bind_port.c
@@ -0,0 +1,18 @@
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+
+#define MYPORT 4000
+
+main()
+{
+  int sockfd;
+
+  struct sockaddr_in my_addr;
+
+  sockfd = socket(AF_INET,SOCK_STREAM,0);
+  my_addr.sin_family=AF_INET;       /* host byte sequance */
+  my_addr.sin_port=htons(MYPORT);   /* internet byte sequence */
+  my_addr.sin_addr.s_addr=inet_addr("166.111.69.52");
+  bzero(&(my_addr.sin_zero),8);     /* erases the data in n bytes of mem */
+  
