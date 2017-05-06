e725c7ad685490f225a8216d058e0c9c2a21d8fc
402696415@qq.com <402696415@qq.com>
2017-05-06 01:59:14 -0500

    dict

---
+++ ch12/dict.c
@@ -0,0 +1,73 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+#define MAXWORD 50
+
+#define DICTSIZ 100
+
+char dict[DICTSIZ][MAXWORD+1]
+
+int nwords=0;
+int nextin(char* cmd,char* word);
+int initw();
+int insertw(const char* word);
+int deletew(const char* word);
+int lookupw(const char* word);
+
+int main(int argc, char* argv[])
+{
+	/* get var from cmd line */
+	char word[MAXWORD+1];
+	char cmd;
+
+	int wrdlen;
+	while(1)
+	{
+		wrdlen = nextin(&cmd,word);
+		if(wrdlen < 0)
+		{
+			exit(0);
+		}
+
+		switch(cmd)
+		{
+			case 'I':
+				initw();
+				printf("Dictionary initalized to empty.\n");
+				break;
+			case 'i':
+				insert(word);
+				printf("%s inserted.\n",word);
+				break;
+			case 'd':
+				if(deletew(word))
+				{
+					printf("%s deleted.\n",word);
+				} else {
+					printf("%s not found.\n",word);
+				}
+				break;
+			case 'l':
+				if(lookupw(word))
+				{
+					printf("%s was found.\n",word);
+				} else {
+					printf("%s was not found.\n",word);
+				}
+				break;
+			case 'q':
+				printf("program quits \n");
+				exit(0);
+			default:
+				printf("command %c invalid. \n",cmd);
+				break;
+		}
+	}
+}
+
+int nextin(char* cmd, char* word)
+{
+	int i, ch;
+
